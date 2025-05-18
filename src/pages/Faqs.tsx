48d9660205f193b6c857087fdb51b5bc95ddf720
craiglordrex <74227496+craiglordrex@users.noreply.github.com>
2025-05-18 10:47:56 +0800

    Fix FAQHeader import and restore header component in Faqs page

--- src/pages/Faqs.tsx
+++ src/pages/Faqs.tsx
@@ -2,17 +2,14 @@ import '.././App.css'
 import Crumbs from '../components/Crumbs';
 import Footer from '../components/Footer';
 import FAQBody from '../sections/FAQBody';
+import FAQHeader from '../sections/FAQHeader';
 // import FAQHeader from '../sections/FAQHeader';
 
 function Faqs() {
   return (
     <div>
-      <Crumbs pageName="FAQs" />
-<<<<<<< HEAD
-      <FAQHeader title="Frequently Asked Questions"/>
-=======
-      {/* <FAQHeader pageName="Frequently Asked Questions" /> */}
->>>>>>> a5b6c6ef8145f820354d4c90e171c75595132212
+<Crumbs pageName="FAQs" />
+<FAQHeader title="Frequently Asked Questions" />
       <FAQBody />
       <Footer />
     </div>
